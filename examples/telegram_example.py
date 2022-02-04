5edbc07b097f357b49676b7c46620bed844d3849
WwWSrJSilverWwW <Sergey081203@ya.ru>
2022-02-04 18:39:38 +0400

    Version 0.2.2.040222

---
+++ examples/telegram_example.py
@@ -0,0 +1,18 @@
+from silverbots.telegram import TelegramBot
+
+bot = TelegramBot("<YOUR_TOKEN>")
+
+
+@bot.handle_message()
+def handle_message(message):
+    chat_id = message.from_.id
+    msg_text = message.text.lower()
+    if "hello" in msg_text or "hi" in msg_text:
+        bot.send_message(chat_id, "Hi, I'm a sample Bot!")
+    elif "buy" in msg_text:
+        bot.send_message(chat_id, "Buy! Hope to see you again!")
+    else:
+        bot.send_message(chat_id, "I can't understand you.")
+
+
+bot.run(debug=True)
